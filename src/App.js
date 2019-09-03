2101243f2791b61f873c961e171b06f431518ae2
brooksmorgan <morgansilencer@gmail.com>
2019-09-03 14:01:27 -0700

    hello

--- src/App.js
+++ src/App.js
@@ -4,6 +4,25 @@ function App() {
   return (
     <div className="App">
       <center><h1>TESTING</h1></center>
+
+      <header> 
+        <div>
+          <ul>HOME</ul>
+        </div>
+
+        <div>
+          <ul>BLOG</ul>
+        </div>
+
+        <div>
+          <ul>STORIS</ul>
+        </div>
+
+        <div>
+          <ul>CONTACT US</ul>
+        </div>
+     
+      </header>
     </div>
   );
 }
